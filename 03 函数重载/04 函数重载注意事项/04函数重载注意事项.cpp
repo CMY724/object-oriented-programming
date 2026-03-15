be172c4f5a2656a42b08f6098f2cbdd50cb453dc
CMY724 <chumingyu724@163.com>
2026-03-15 15:30:19 +0800

    Rename 03 函数重载/04 函数重载注意事项/04 函数重载注意事项/04函数重载注意事项.cpp to 03 函数重载/04 函数重载注意事项/04函数重载注意事项.cpp

---
+++ 03 函数重载/04 函数重载注意事项/04函数重载注意事项.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+
+//函数重载注意事项
+//1、引用作为重载条件
+
+void func(int& a)
+{
+	cout << "func (int &a) 调用 " << endl;
+}
+
+void func(const int& a)//const修饰的引用可以绑定到常量上，非const修饰的引用只能绑定到非常量上
+{
+	cout << "func (const int &a) 调用 " << endl;
+}
+
+
+//2、函数重载碰到函数默认参数
+
+void func2(int a, int b = 10)
+{
+	cout << "func2(int a, int b = 10) 调用" << endl;
+}
+
+void func2(int a)
+{
+	cout << "func2(int a) 调用" << endl;
+}
+
+int main() {
+
+	int a = 10;
+	func(a); //调用无const
+	func(10);//调用有const
+
+
+	//func2(10); //碰到默认参数产生歧义，需要避免
+
+	system("pause");
+
+	return 0;
+}
