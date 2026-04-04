299df8c8bd1e8ac67a491c74c3075bfef162afa4
CMY724 <chumingyu724@163.com>
2026-04-04 13:42:11 +0800

    Create 01封装的意义.cpp

---
+++ 04 类和对象/01 封装/01 封装/01 封装/01封装的意义.cpp
@@ -0,0 +1,150 @@
+﻿#include <iostream>
+using namespace std;
+//封装是C++面向对象三大特性之一
+
+//封装的意义：
+
+//将属性和行为作为一个整体，表现生活中的事物
+//将属性和行为加以权限控制
+
+
+//封装意义一：
+
+//在设计类的时候，属性和行为写在一起，表现事物
+
+//语法： class 类名{ 访问权限： 属性 / 行为 }; 
+
+
+
+//示例1:设计一个圆类，求圆的周长
+
+
+//圆周率
+const double PI = 3.14;
+
+//1、封装的意义
+//将属性和行为作为一个整体，用来表现生活中的事物
+
+//封装一个圆类，求圆的周长
+//class代表设计一个类，后面跟着的是类名
+class Circle
+{
+public:  //访问权限  公共的权限
+
+	//属性
+	int m_r;//半径
+
+	//行为
+	//获取到圆的周长
+	double calculateZC()
+	{
+		//2 * pi  * r
+		//获取圆的周长
+		return  2 * PI * m_r;
+	}
+};
+
+int main() {
+
+	//通过圆类，创建圆的对象
+	// c1就是一个具体的圆
+	Circle c1;
+	c1.m_r = 10; //给圆对象的半径 进行赋值操作
+
+	//2 * pi * 10 = = 62.8
+	cout << "圆的周长为： " << c1.calculateZC() << endl;
+
+	system("pause");
+
+	return 0;
+}
+
+
+
+//示例2：设计一个学生类，属性有姓名和学号，可以给姓名和学号赋值，可以显示学生的姓名和学号
+
+
+
+//学生类
+class Student {
+public:
+	void setName(string name) {
+		m_name = name;
+	}
+	void setID(int id) {
+		m_id = id;
+	}
+
+	void showStudent() {
+		cout << "name:" << m_name << " ID:" << m_id << endl;
+	}
+public:
+	string m_name;
+	int m_id;
+};
+
+int main() {
+
+	Student stu;
+	stu.setName("德玛西亚");
+	stu.setID(250);
+	stu.showStudent();
+
+	system("pause");
+
+	return 0;
+} 
+
+//封装意义二：
+
+//类在设计时，可以把属性和行为放在不同的权限下，加以控制
+
+//访问权限有三种：
+
+
+
+//1. public        公共权限
+//2. protected     保护权限
+//3. private       私有权限
+
+
+
+//三种权限
+//公共权限  public     类内可以访问  类外可以访问
+//保护权限  protected  类内可以访问  类外不可以访问
+//私有权限  private    类内可以访问  类外不可以访问
+
+class Person
+{
+	//姓名  公共权限
+public:
+	string m_Name;
+
+	//汽车  保护权限
+protected:
+	string m_Car;
+
+	//银行卡密码  私有权限
+private:
+	int m_Password;
+
+public:
+	void func()
+	{
+		m_Name = "张三";
+		m_Car = "拖拉机";
+		m_Password = 123456;
+	}
+};
+
+int main() {
+
+	Person p;
+	p.m_Name = "李四";
+	//p.m_Car = "奔驰";  //保护权限类外访问不到
+	//p.m_Password = 123; //私有权限类外访问不到
+
+	system("pause");
+
+	return 0;
+}
