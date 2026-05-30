c3af72dd9a5d7908070ee37d6f777b3c6f8ea08c
CMY724 <chumingyu724@163.com>
2026-05-30 22:52:26 +0800

    提交代码

--- 04 类和对象/06 继承/03 继承中的对象模型/03 继承中的对象模型/03继承中的对象模型.cpp
+++ 04 类和对象/06 继承/03 继承中的对象模型/03 继承中的对象模型/03继承中的对象模型.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-//继承中的对象模型
+// 继承中的对象模型
 
 class Base
 {
@@ -24,15 +24,24 @@ public:
 
 };
 
+//利用开发人员命令提示工具查看对象模型
+//跳转盘符 F:
+//跳转文件路径 cd 具体路径下
+//查看命名
+// cl /d1 reportSingleClassLayout类名 文件名
+
 void test01()
 {
+	//父类中所有 非静态成员属性 都会被子类继承下去 
+	//父类中私有的成员属性是被编译器给隐藏了 因此访问不到 但是确实被继承下去了
 	cout << "size of Son = " << sizeof(Son) << endl;
+	//16
 }
 
 int main()
 {
 
-
+	test01();
 
 	return 0;
 }
