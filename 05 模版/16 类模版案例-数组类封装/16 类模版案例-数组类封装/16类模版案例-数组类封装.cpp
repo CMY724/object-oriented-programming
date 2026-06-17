21ec656274b22927bf9175da8212c09c0437c78a
CMY724 <chumingyu724@163.com>
2026-06-17 18:32:14 +0800

    提交代码

--- 05 模版/16 类模版案例-数组类封装/16 类模版案例-数组类封装/16类模版案例-数组类封装.cpp
+++ 05 模版/16 类模版案例-数组类封装/16 类模版案例-数组类封装/16类模版案例-数组类封装.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "MyArray.hpp"
+#include <string>
 using namespace std;
 
 void test01()
@@ -14,10 +15,104 @@ void test01()
 
 }
 
+void printIntArray(MyArray <int>& arr)
+{
+	for (int i = 0; i < arr.getSize(); i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
+
+void test02()
+{
+	MyArray <int>arr1(5);
+	for (int i = 0; i < 5; i++)
+	{
+		//利用尾插法向数组中插入数据
+		arr1.Push_Back(i);
+	}
+
+	cout << "arr1的打印输出为: " << endl;
+	printIntArray(arr1);
+
+	cout << "arr1的容量为: " << arr1.getCapacity() << endl;
+	cout << "arr1的大小为: " << arr1.getSize() << endl;
+
+
+	MyArray <int>arr2(arr1);
+
+	cout << "arr2的打印输出为: " << endl;
+	printIntArray(arr2);
+
+	//尾删
+	arr2.Pop_Back();
+	
+	cout << "arr2尾删后: " << endl;
+	cout << "arr2的容量为: " << arr2.getCapacity() << endl;
+	cout << "arr2的大小为: " << arr2.getSize() << endl;
+}
+
+
+//测试自定义数据类型
+
+class Person
+{
+public:
+
+	Person() {};
+	Person(string name, int age)
+	{
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+
+
+	string m_Name;
+	int m_Age;
+
+};
+
+void printPersonArray(MyArray<Person>& arr)
+{
+	for (int i = 0; i < arr.getSize(); i++)
+	{
+		cout << "姓名: " << arr[i].m_Name << "年龄: " << arr[i].m_Age << endl;
+	}
+}
+
+void test03()
+{
+	MyArray<Person>arr(10);
+	Person p1("孙悟空", 999);
+	Person p2("韩信", 30);
+	Person p3("妲己", 20);
+	Person p4("赵云", 25);
+	Person p5("安其拉", 27);
+
+	//将数据插入到数组中
+	arr.Push_Back(p1);
+	arr.Push_Back(p2);
+	arr.Push_Back(p3);
+	arr.Push_Back(p4);
+	arr.Push_Back(p5);
+
+	//打印数组
+	printPersonArray(arr);
+
+	//容量大小
+
+	cout << "arr的容量为: " << arr.getCapacity() << endl;
+	cout << "arr的大小为: " << arr.getSize() << endl;
+
+}
+
+
 
 int main()
 {
-	test01();
-
+	//test01();
+	//test02();
+	test03();
 	return 0;
 }
--- 05 模版/16 类模版案例-数组类封装/16 类模版案例-数组类封装/MyArray.hpp
+++ 05 模版/16 类模版案例-数组类封装/16 类模版案例-数组类封装/MyArray.hpp
@@ -64,6 +64,53 @@ public:
 	}
 
 
+	//尾插法
+	
+	void Push_Back(const T& val)
+	{
+		//判断容量是否等于大小
+		if (this->m_Capacity == this->m_Size)
+		{
+			return;
+		}
+
+		this->pAddress[this->m_Size] = val;//在数组末尾插入数据
+		this->m_Size++;//更新数组大小
+	}
+
+	//尾删法
+
+	void Pop_Back()
+	{
+		//让用户访问不到最后一个元素 即为尾删 逻辑删除
+		if (this->m_Size == 0)
+		{
+			return ;
+		}
+		
+		this->m_Size--;
+
+	}
+
+	//通过下标方式访问数组中元素
+	T & operator[](int index)
+	{
+		return this->pAddress[index];
+	}
+
+	//返回数组的容量
+	int getCapacity()
+	{
+		return this->m_Capacity;
+	}
+
+	//返回数组大小
+	int getSize()
+	{
+		return this->m_Size;
+	}
+
+
 	//析构函数
 	~MyArray()
 	{
