685ab9236f516f010c9040f5b8d47937ecd67e27
CMY724 <chumingyu724@163.com>
2026-06-22 23:47:43 +0800

    提交代码

---
+++ 06 文件/01 文本文件/02 读文件/02 读文件/02读文件.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//文本文件 读文件
+
+void test01()
+{
+	//1.包含头文件
+
+	//2.创建流对象
+	ifstream ifs;
+
+	//3.打开文件 并且判断是否打开成功
+
+	ifs.open("test.txt", ios::in);
+
+	if (!ifs.is_open())
+	{
+		cout << "文件打开失败了" << endl;
+		return;
+	}
+
+	//4.读数据
+
+	//第一种
+	/*char buf[1024] = { 0 };
+	while (ifs >> buf)
+	{
+		cout << buf << endl;
+	}*/
+
+	//第二种
+	/*char buf[1024] = { 0 };
+	while (ifs.getline(buf, sizeof(buf)))
+	{
+		cout << buf << endl;
+	}*/
+
+	//第三种
+	/*string buf;
+
+	while (getline(ifs, buf))
+	{
+		cout << buf << endl;
+	}*/
+
+	//第四种
+	char c;
+	while ((c = ifs.get()) != EOF) //EOF是 end of file
+	{
+		cout << c;
+	}
+
+
+	//5.关闭文件
+	ifs.close();
+}
+
+int main()
+{
+	test01();
+
+	return 0;
+}
+
+//利用is_open可以判断文件是否打开成功
